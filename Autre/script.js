4822283015046a2a8b932f1d39018fb655c345dc
D4L1IR3Tr0 <41015278+D4L1IR3Tr0@users.noreply.github.com>
2022-04-19 12:06:14 +0100

    Add files via upload

---
+++ Autre/script.js
@@ -0,0 +1,15 @@
+
+
+    function Recuperation(){
+        var Nom = document.getElementById('FieldNom').value;
+        var Prenom = document.getElementById('FieldPNom').value;
+        const DTN = document.getElementById('FieldDateN').value;
+        //const NumU = document.getElementById('FieldNumU').value;
+        const MDP = document.getElementById('FieldMDP').value;
+        const Mail = document.getElementById('FieldMail').value;
+
+
+        console.log(Nom);
+        console.log(Prenom);
+        console.log(Mail);
+}
