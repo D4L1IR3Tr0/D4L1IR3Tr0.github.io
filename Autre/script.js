a641f6f83217b43b3f44776f11c97c8c9fac71e9
D4L1IR3Tr0 <41015278+D4L1IR3Tr0@users.noreply.github.com>
2022-04-20 22:35:51 +0100

    Add files via upload

--- Autre/script.js
+++ Autre/script.js
@@ -1,15 +1,30 @@
 
 
     function Recuperation(){
-        var Nom = document.getElementById('FieldNom').value;
-        var Prenom = document.getElementById('FieldPNom').value;
-        const DTN = document.getElementById('FieldDateN').value;
+        let Nom, Prenom, dateNaissance, passe, Mail;
         //const NumU = document.getElementById('FieldNumU').value;
-        const MDP = document.getElementById('FieldMDP').value;
-        const Mail = document.getElementById('FieldMail').value;
+
+        Nom = document.getElementById('FieldNom').value;
+        Prenom = document.getElementById('FieldPNom').value;
+        dateNaissance = document.getElementById('FieldDateN').value;
+        passe = document.getElementById('FieldMDP').value;
+        Mail = document.getElementById('FieldMail').value;
+
+        localStorage.setItem('Nom',Nom);
+        localStorage.setItem('Prenom',Prenom);
+        localStorage.setItem('dateNaissance',dateNaissance);
+        localStorage.setItem('passe',passe);
+        localStorage.setItem("Mail",Mail);
 
 
-        console.log(Nom);
-        console.log(Prenom);
-        console.log(Mail);
+
 }
+
+    function Connection(){
+        const mailCo = document.getElementById('FieldMailCo').value;
+        const passeCo = document.getElementById('FieldMDPCo').value;
+
+        if(mailCo === localStorage.getItem('Mail') && passeCo === localStorage.getItem('passe')){
+            window.location="main.html";
+        }
+    }
