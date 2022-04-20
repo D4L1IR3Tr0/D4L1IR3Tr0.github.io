587d769d7ae148db126872d64592eaca34670b84
D4L1IR3Tr0 <41015278+D4L1IR3Tr0@users.noreply.github.com>
2022-04-20 23:04:54 +0100

    Add files via upload

--- Autre/script.js
+++ Autre/script.js
@@ -10,12 +10,32 @@
         passe = document.getElementById('FieldMDP').value;
         Mail = document.getElementById('FieldMail').value;
 
-        localStorage.setItem('Nom',Nom);
-        localStorage.setItem('Prenom',Prenom);
-        localStorage.setItem('dateNaissance',dateNaissance);
-        localStorage.setItem('passe',passe);
-        localStorage.setItem("Mail",Mail);
-
+        const letters = /^[A-Za-z]+$/;
+        const email_val = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        const dateFormat = /^\d{2}\/\d{2}\/\d{4}$/;
+//other validations required code
+        if(Nom===''||Mail===''||passe===''||dateNaissance===''){
+            alert("Les champs vides ne sonts pas autorisés");
+        }
+        else if(!letters.test(Nom) || !letters.test(Prenom))
+        {
+            alert('Le nom ne peux contenir que des lettres');
+        }
+        else if (!email_val.test(Mail))
+        {
+            alert('Mail invalide');
+        }
+        else if (!dateFormat.test(dateNaissance)){
+            alert('La date de naissance doit correspondre au format indiqué')
+        }
+        else {
+            localStorage.setItem('Nom', Nom);
+            localStorage.setItem('Prenom', Prenom);
+            localStorage.setItem('dateNaissance', dateNaissance);
+            localStorage.setItem('passe', passe);
+            localStorage.setItem("Mail", Mail);
+            alert('Compte crée')
+        }
 
 
 }
